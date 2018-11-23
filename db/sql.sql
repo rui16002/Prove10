8348192aba05b26dd741beebd9cc422761042392
rui16002 <email.jruiz@yahoo.com>
2018-11-23 19:29:40 +0100

    prove 10

---
+++ db/sql.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE expensetracker;
+\c expensetracker;
+
+CREATE TABLE movements
+(
+	movementID SERIAL PRIMARY KEY NOT NULL,
+	typeID INT REFERENCES types(typeID) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	movementDate date NOT NULL,
+	amount float NOT NULL
+);
+
+INSERT INTO movements(typeID, name, movementDate, amount) VALUES
+  (1, 'Salary', '1878-08-28',1850.0)
+  (2, 'Tithings', '1849-08-06',185.0),
+  (2, 'Offering', '1849-08-06',18.5);
+  
+ CREATE TABLE types
+ (
+	typeID SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(50) NOT NULL
+);
+	
+INSERT INTO types(name) VALUES
+("Income"),
+("Donations"),
+("Education"),
+("Transportation"),
+("Loans"),
+("Credit cards"),
+("Health"),
+("Home");
+
+CREATE USER rui16002 WITH PASSWORD 'rui16002';
+GRANT SELECT, INSERT, UPDATE ON movement TO rui16002;
+GRANT USAGE, SELECT ON SEQUENCE movement_id_seq TO rui16002;
+
+GRANT SELECT, INSERT, UPDATE ON type TO rui16002;
+GRANT USAGE, SELECT ON SEQUENCE type_id_seq TO rui16002;
--- server.js
+++ server.js
@@ -2,112 +2,173 @@ var express = require('express');
 var app = express();
 
 const { Pool } = require("pg");
-const connectionString = process.env.DATABASE_URL || "postgres://steven:password@localhost:5432/familyhistory";
+const connectionString = process.env.DATABASE_URL || "postgres://rui16002:rui16002@localhost:5432/expensetracker";
 const pool = new Pool({connectionString: connectionString});
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/getPerson', getPerson);
-app.get('/getParents', getParents);
-app.get('/getChildren', getChildren);
+//Use POST method here
+app.post('/AddMovement', addMovement);
+
+//Use GET method here
+app.get('/GetMovement', getMovement);
+app.get('/GetAvg', getAvg);
+app.get('/GetTotal', getTotal);
+
+//Use PUT Method here
+app.put('/ModifyMovement', modifyMovement);
+
+//Use DELETE method here
+app.delete('/DeleteMovement', deleteMovement);
+
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
-function getPerson(req, res) {
-	var id = req.query.id;
+/* ----- addMovement-----------------------------------
+Data from client: type, date, name, amount
+Data returned: ExpenseID or null
+-------------------------------------------------------*/
+function addMovement(req, res) {
+	var query = req.query;
+	var type = query.type;
+	var date = query.date;
+	var name = query.name;
+	var amount = query.amount;
+	console.log("Adding movement: " + name);
+	var sql = "INSERT INTO movements(typeID, name, movementDate, amount) VALUES ($1, $2, $3, $4)";
+	var params = [type, name, date, amount];
 
-	 getPersonFromDb(id, function(error, result) {
-		if (error || result == null || result.length != 1) {
-			 res.status(500).json({success: false, data: error});
-		 } else {
-			 var person = result[0];
-			 res.status(200).json(result[0]);
+	dbTransaction(sql, params, function(error, result) {
+		if (error || result == null) {
+			res.writeHead(500);
+		} else {
+			res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(result));
 		}
-	 });
+	})
 }
-function getPersonFromDb(id, callback) {
-	console.log("Getting person from DB with id: " + id);
+/* ----- getMovement----------------------------------- 
+Data from client: Month, Type
+Data returned: List of Expenses for the given type
+-------------------------------------------------------*/
+function getMovement(req, res) {
+	var query = req.query;
+	var d = new Date(2018, query.month, 0);
+    var enddate = '2018/'+ query.month +'/' + d.getDate();
+	var startdate = '2018/' + query.month + '/01';
+	var type = query.type;
+	var movements = [];
+	console.log("getting movements of " + type + " from " + month);
 
-		var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
-	var params = [id];
+	var sql = "SELECT typeID, name, movementDate, amount FROM movements WHERE typeID = $1 AND movementDate BETWEEN $2 AND $3";
+	var params = [type, startdate, enddate];
 
+	dbTransaction(sql, params, function(error, result) {
+		 if (error || result == null) {
+		 	 res.status(500).json({success: false, data: error});
+		  } else {
+			 res.status(200).json(result.rows);
+		 }
+	})
+}
+/* ----- getAvg----------------------------------------
+Data from client: Type
+Data returned: Avg expense on a given category
+-------------------------------------------------------*/
+function getAvg(req, res) {
+	var type = req.query.type;
+	console.log("Getting avg from type: " + type);
+
+	var sql = "SELECT AVG(amount) FROM movements WHERE typeID = $1";
+	var params = [type];
+
+	dbTransaction(sql, params, function(error, result) {
+		 if (error || result == null) {
+		 	 res.status(500).json({success: false, data: error});
+		  } else {
+			 res.status(200).json(result.rows);
+		 }
+	})
+}
+/* ----- getTotal--------------------------------------
+Data from client: Type, Month
+Data returned: Total expenses on a given category
+-------------------------------------------------------*/
+function getTotal(req, res) {
+	var query = req.query;
+	var d = new Date(2018, query.month, 0);
+    var enddate = '2018/'+ query.month +'/' + d.getDate();
+	var startdate = '2018/' + query.month + '/01';
+	var type = query.type;
+	var total = 100.6;
+	console.log("getting total from type: "+ type);
+
+	var sql = "SELECT SUM(amount) FROM movements WHERE typeID = $1 AND movementDate BETWEEN $2 AND $3";
+	var params = [type];
+
+	dbTransaction(sql, params, function(error, result) {
+		 if (error || result == null) {
+		 	 res.status(500).json({success: false, data: error});
+		  } else {
+			 res.status(200).json(result.rows);
+		 }
+	})
+}
+/* ----- modifyMovement---------------------------------
+Data from client: MovementID, type, name, amount
+Data returned: true/false depending on successful modification
+-------------------------------------------------------*/
+function modifyMovement(req, res) {
+	var query = req.query;
+	var movementID = query.movementID;
+	var type = query.type;
+	var name = query.name;
+	var date = query.date;
+	var amount = query.amount;
+	console.log("Modifying movement: "+ name);
+	var sql = "UPDATE movements SET typeID = $1, name = $2, movementDate = $3, amount = $4 WHERE movementID = $5";
+	var params = [type, name, date, amount, movementID];
+
+	dbTransaction(sql, params, function(error, result) {
+		 if (error || result == null) {
+		 	 res.status(500).json({success: false, data: error});
+		  } else {
+			 res.status(200).json(result.rows);
+		 }
+	})
+	}
+/* ----- deleteMovement--------------------------------
+Data from client: MovementID
+Data returned: true/false depending on successful deletion
+-------------------------------------------------------*/
+function deleteMovement(req, res) {
+	var movementID = req.query.movementID;
+	console.log("Deleting movement: " + movementID);
+	var sql = "DELETE FROM movements WHERE movementID = $1";
+	var params = [movementID];
+
+	dbTransaction(sql, params, function(error, result) {
+		 if (error || result == null) {
+		 	 res.status(500).json({success: false, data: error});
+		  } else {
+			 res.status(200).json(result.rows);
+		 }
+	})
+	}
+
+function dbTransaction(sql, params, callback) {
 	pool.query(sql, params, function(err, result) {
 		if (err) {
-			console.log("Error in query: ")
-			console.log(err);
+			console.log("Transaction failed: " + err);
 			callback(err, null);
 		}
 
-		console.log("Found result: " + JSON.stringify(result.rows));
+		console.log("Transaction successful: " + JSON.stringify(result.rows));
 
 		callback(null, result.rows);
 	});
 
-} 
-
-
-function getParents(req, res) {
-	var id = req.query.id;
-
-	 getParentsFromDb(id, function(error, result) {
-		if (error || result == null || result.length === 0) {
-			 res.status(500).json({success: false, data: error});
-		 } else {
-			 res.status(200).json(result);
-		}
-	 });
 }
-function getParentsFromDb(id, callback) {
-	console.log("Getting person from DB with id: " + id);
-
-		var sql = "SELECT person.first AS firstname, person.last AS lastname FROM relationship INNER JOIN person ON other_person_id = person.id WHERE person_id = $1::int AND relationship_type = 0";
-	var params = [id];
-
-	pool.query(sql, params, function(err, result) {
-		if (err) {
-			console.log("Error in query: ")
-			console.log(err);
-			callback(err, null);
-		}
-
-		console.log("Found result: " + JSON.stringify(result.rows));
-
-		callback(null, result.rows);
-	});
-
-} 
-
-
-function getChildren(req, res) {
-	var id = req.query.id;
-
-	 getChildrenFromDb(id, function(error, result) {
-		if (error || result == null || result.length === 0) {
-			 res.status(500).json({success: false, data: error});
-		 } else {
-			 res.status(200).json(result);
-		}
-	 });
-}
-function getChildrenFromDb(id, callback) {
-	console.log("Getting person from DB with id: " + id);
-
-		var sql = "SELECT person.first AS firstname, person.last AS lastname FROM relationship INNER JOIN person ON other_person_id = person.id WHERE person_id = $1::int AND relationship_type = 1";
-	var params = [id];
-
-	pool.query(sql, params, function(err, result) {
-		if (err) {
-			console.log("Error in query: ")
-			console.log(err);
-			callback(err, null);
-		}
-
-		console.log("Found result: " + JSON.stringify(result.rows));
-
-		callback(null, result.rows);
-	});
-
-} 
