e9ca47a630619336c0d52a57d0c5bada6dcdb726
rui16002 <email.jruiz@yahoo.com>
2018-12-02 02:23:30 +0100

    prove10 - trying to set content as json to be able to parse

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/', function (req, res) {
-  res.redirect('/ExpenseTracker.html');
-})
-
 //Use POST method here
 app.post('/AddMovement', addMovement);
 
