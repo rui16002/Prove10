4b622f7f4ab29e274b179f57ade396adf24fb38d
rui16002 <email.jruiz@yahoo.com>
2018-12-02 01:58:30 +0100

    prove10 - trying to set content as json to be able to parse

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+//https://stackoverflow.com/questions/20035101/why-does-my-javascript-get-a-no-access-control-allow-origin-header-is-present
+//Not to be used in production
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //Use POST method here
 app.post('/AddMovement', addMovement);
 
